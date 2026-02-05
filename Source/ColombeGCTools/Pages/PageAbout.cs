30365019e334c89d75e3e3c8e59f6fabd6dadc07
Stellarysian <152004014+Stellarysian@users.noreply.github.com>
2026-02-06 05:28:33 +0800

    Add files via upload

---
+++ Source/ColombeGCTools/Pages/PageAbout.cs
@@ -0,0 +1,51 @@
+﻿/**
+ *  Grasscutter Tools
+ *  Copyright (C) 2022 Stellarysian
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ **/
+
+using System.Windows.Forms;
+using ColombeGCTools.Properties;
+using ColombeGCTools.Utils;
+
+namespace ColombeGCTools.Pages
+{
+    internal partial class PageAbout : BasePage
+    {
+        public override string Text => Resources.PageAboutTitle;
+
+        public PageAbout()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 点击Github链接时触发(未定义,所以点击后无跳转效果
+        /// </summary>
+        private void LnkGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            UIUtil.OpenURL("");
+        }
+
+        /// <summary>
+        /// 点击查看服务器聊天插件时触发(未定义,所以点击后无跳转效果
+        /// </summary>
+        private void LnkOpenChat_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            UIUtil.OpenURL("");
+        }
+    }
+}
